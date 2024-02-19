be3f4dda7491e477fe48bca137bef1fc2523e168
Prajwal Shrestha <pjmessi25@icloud.com>
2024-02-19 14:05:33 +0545

    Update harpoon keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -17,14 +17,13 @@ harpoon:setup()
 --vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 
 -- atomicmage's keymap
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
-
--- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
 vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end)
+
