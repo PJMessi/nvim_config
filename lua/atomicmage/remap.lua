311afa2270cc1619829e8054b0e96b20e7dcdc0b
Prajwal Shrestha <pjmessi25@icloud.com>
2024-02-19 14:10:42 +0545

    Add split pane navigation remaps

--- lua/atomicmage/remap.lua
+++ lua/atomicmage/remap.lua
@@ -55,5 +55,10 @@ end)
 --  atomicmage's keymaps
 vim.keymap.set("i", "<M-BS>", "<C-w>", { noremap = true })
 
+-- splitpane navigation
+vim.keymap.set("n", "<leader>wk", '<C-w>k')
+vim.keymap.set("n", "<leader>wj", '<C-w>k')
+vim.keymap.set("n", "<leader>wl", '<C-w>l')
+vim.keymap.set("n", "<leader>wh", '<C-w>h')
 
 
