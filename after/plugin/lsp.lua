db53578d5bb16535d32acf6bd60e85c21fd7a04c
Prajwal Shrestha <pjmessi25@icloud.com>
2024-02-22 09:56:23 +0545

    Fix indentation

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,7 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
-
+    local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -19,7 +18,17 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {'tsserver', 'lua_ls', 'rust_analyzer', 'eslint', 'gopls', 'dockerls', 'docker_compose_language_service', 'solargraph' },
+    ensure_installed = {
+        'tsserver',
+        'lua_ls',
+        'rust_analyzer',
+        'eslint',
+        'gopls',
+        'dockerls',
+        'docker_compose_language_service',
+        'solargraph',
+    },
+
     handlers = {
         lsp_zero.default_setup,
         lua_ls = function()
@@ -30,7 +39,7 @@ require('mason-lspconfig').setup({
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
@@ -38,19 +47,20 @@ require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
     sources = {
-        {name = 'path'},
-        {name = 'nvim_lsp'},
-        {name = 'nvim_lua'},
-        {name = 'luasnip', keyword_length = 2},
-        {name = 'buffer', keyword_length = 3},
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip', keyword_length = 2 },
+        { name = 'buffer',  keyword_length = 3 },
     },
     formatting = lsp_zero.cmp_format(),
     mapping = cmp.mapping.preset.insert({
         ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-        -- ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-CR>'] = cmp.mapping.confirm({ select = true }), -- atomicmage's keymap
+
+        -- original: ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-CR>'] = cmp.mapping.confirm({ select = true }),
+
         ['<C-Space>'] = cmp.mapping.complete(),
     }),
 })
-
